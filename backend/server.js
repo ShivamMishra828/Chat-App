5d778356ae2e5a8857ad473db1c5f6ad9f057082
Shivam <shivam9878522087.com>
2023-03-08 18:31:58 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,153 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const path = require("path");
+
+// const { Server } = require("socket.io");
+
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  console.log("UNCAUGHT Exception! Shutting down ...");
+  process.exit(1); // Exit Code 1 indicates that a container shut down, either because of an application failure.
+});
+
+const app = require("./app");
+
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io"); // Add this
+const { promisify } = require("util");
+const User = require("./models/user");
+const FriendRequest = require("./models/friendRequest");
+
+// Add this
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true, // The underlying MongoDB driver has deprecated their current connection string parser. Because this is a major change, they added the useNewUrlParser flag to allow users to fall back to the old parser if they find a bug in the new parser.
+    // useCreateIndex: true, // Again previously MongoDB used an ensureIndex function call to ensure that Indexes exist and, if they didn't, to create one. This too was deprecated in favour of createIndex . the useCreateIndex option ensures that you are using the new function calls.
+    // useFindAndModify: false, // findAndModify is deprecated. Use findOneAndUpdate, findOneAndReplace or findOneAndDelete instead.
+    // useUnifiedTopology: true, // Set to true to opt in to using the MongoDB driver's new connection management engine. You should set this option to true , except for the unlikely case that it prevents you from maintaining a stable connection.
+  })
+  .then((con) => {
+    console.log("DB Connection successful");
+  });
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`App running on port ${port} ...`);
+});
+
+// Add this
+// Listen for when the client connects via socket.io-client
+io.on("connection", async (socket) => {
+  // console.log(JSON.stringify(socket.handshake.query));
+  const user_id = socket.handshake.query["user_id"];
+
+  // console.log(`User connected ${socket.id}`);
+
+  // console.log(Boolean(user_id), user_id);
+
+  if (Boolean(user_id)) {
+    await User.findByIdAndUpdate(user_id, {
+      socket_id: socket.id,
+      status: "Online",
+    });
+  }
+
+  // We can write our socket event listeners in here...
+  socket.on("friend_request", async (data) => {
+    // console.log(data.to);
+
+    const to = await User.findById(data.to).select("socket_id");
+    const from = await User.findById(data.from).select("socket_id");
+
+    // create a friend request
+    await FriendRequest.create({
+      sender: data.from,
+      recipient: data.to,
+    });
+    // emit event request received to recipient
+    io.to(to.socket_id).emit("new_friend_request", {
+      message: "New friend request received",
+    });
+    io.to(from.socket_id).emit("request_sent", {
+      message: "Request Sent successfully!",
+    });
+  });
+
+  socket.on("accept_request", async (data) => {
+    // accept friend request => add ref of each other in friends array
+    // console.log(data);
+    const request_doc = await FriendRequest.findById(data.request_id);
+
+    // console.log(request_doc);
+
+    const sender = await User.findById(request_doc.sender);
+    const receiver = await User.findById(request_doc.recipient);
+
+    sender.friends.push(request_doc.recipient);
+    receiver.friends.push(request_doc.sender);
+
+    await receiver.save({ new: true, validateModifiedOnly: true });
+    await sender.save({ new: true, validateModifiedOnly: true });
+
+    await FriendRequest.findByIdAndDelete(data.request_id);
+
+    // delete this request doc
+    // emit event to both of them
+
+    // emit event request accepted to both
+    io.to(sender.socket_id).emit("request_accepted", {
+      message: "Friend Request Accepted",
+    });
+    io.to(receiver.socket_id).emit("request_accepted", {
+      message: "Friend Request Accepted",
+    });
+  });
+
+  socket.on("text_message", (data) => {});
+
+  socket.on("file_message", (data) => {
+    const fileExtension = path.extname(data.file.name);
+
+    const fileName = `${Date.now()}_${Math.floor(
+      Math.random() * 10000
+    )}${fileExtension}`;
+  });
+
+  socket.on("end", async (data) => {
+    if (data.user_id) {
+      await User.findByIdAndUpdate(data.user_id, { status: "Offline" });
+    }
+
+    console.log("closing connection");
+    socket.disconnect(0);
+  });
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  console.log("UNHANDLED REJECTION! Shutting down ...");
+  server.close(() => {
+    process.exit(1); //  Exit Code 1 indicates that a container shut down, either because of an application failure.
+  });
+});
---
+++ frontend/src/layouts/dashboard/index.js
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+import { Stack } from "@mui/material";
+import { Navigate, Outlet } from "react-router-dom";
+import useResponsive from "../../hooks/useResponsive";
+import SideNav from "./SideNav";
+import { useDispatch, useSelector } from "react-redux";
+import { showSnackbar } from "../../redux/slices/app";
+import { socket, connectSocket } from "../../socket";
+
+const DashboardLayout = () => {
+  const isDesktop = useResponsive("up", "md");
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const user_id = window.localStorage.getItem("user_id");
+
+  // console.log(socket);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.onload = function () {
+        if (!window.location.hash) {
+          window.location = window.location + "#loaded";
+          window.location.reload();
+        }
+      };
+
+      window.onload();
+
+      if (!socket) {
+        connectSocket(user_id);
+      }
+      socket.on("new_friend_request", (data) => {
+        dispatch(
+          showSnackbar({
+            severity: "success",
+            message: "New friend request received",
+          })
+        );
+      });
+
+      socket.on("request_accepted", (data) => {
+        dispatch(
+          showSnackbar({
+            severity: "success",
+            message: "Friend Request Accepted",
+          })
+        );
+      });
+
+      socket.on("request_sent", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }));
+      });
+    }
+
+    // Remove event listener on component unmount
+    return () => {
+      socket.off("new_friend_request");
+      socket.off("request_accepted");
+    };
+  }, [isLoggedIn, socket]);
+
+  if (!isLoggedIn) {
+    return <Navigate to={"/auth/login"} />;
+  }
+
+  return (
+    <>
+      <Stack direction="row">
+        {isDesktop && (
+          // SideBar
+          <SideNav />
+        )}
+
+        <Outlet />
+      </Stack>
+    </>
+  );
+};
+
+export default DashboardLayout;
